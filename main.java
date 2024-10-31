e8376e5426296af1acaecd43af3a8ef149d3af0b
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-10-31 08:14:43 -0300

    .

---
+++ main.java
@@ -0,0 +1,12 @@
+class Helloworld {
+  public static void main ( String [] args ){
+    System.out.println(" Esse e o Screen Match");
+    System.out.println(" Filme: Red crescer é uma fera");
+    
+    int ano = 2022;
+    boolean incluindoNOPLANO= true;
+    double notaFilme= 9.0;
+    
+    System.out.println("Ano de lançamento " + ano);
+  }
+}
