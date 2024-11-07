2cf3b8fda0adb376092d838fc13f4a918a55aff2
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-11-07 08:10:34 -0300

    .

--- main.java
+++ main.java
@@ -1,12 +1,21 @@
-class Helloworld {
-  public static void main ( String [] args ){
-    System.out.println(" Esse e o Screen Match");
-    System.out.println(" Filme: Red crescer é uma fera");
-    
-    int ano = 2022;
-    boolean incluindoNOPLANO= true;
-    double notaFilme= 9.0;
-    
-    System.out.println("Ano de lançamento " + ano);
+class HelloWorld {
+  public static void main(String[] args) {
+   System.out.println("Esse e o Screen Match");
+   System.out.println("Filme: O");
+   
+   int ano = 2023;
+   
+   
+   boolean incluidoNoPlano = true;
+   double notaFilme = 9.5;
+
+  System.out.println("Ano de lancamento:" + ano);
+   
+   double media = (8.5 + 9.0. + 9.5)/3;
+   System.out.println("A media das notas deste filme e:" + media);
+
+   String sinopse;
+   sinopse = "Oppenheimer é um filme histórico de drama dirigido por Christopher Nolan e baseado no livro biográfico vencedor do Prêmio Pulitzer, Prometeu Americano: O Triunfo e a Tragédia de J. Robert Oppenheimer, escrito por Kai Bird e Martin J. Sherwin. Ambientado na Segunda Guerra Mundial, o longa acompanha a vida de J. Robert Oppenheimer (Cillian Murphy), físico teórico da Universidade da Califórnia e diretor do Laboratório de Los Alamos durante o Projeto Manhattan - que tinha a missão de projetar e construir as primeiras bombas atômicas. A trama acompanha o físico e um grupo formado por outros cientistas ao longo do processo de desenvolvimento da arma nuclear que foi responsável pelas tragédias nas cidades de Hiroshima e Nagasaki, no Japão, em 1945.\n";
+   System.out.println(sinopse);
   }
 }
